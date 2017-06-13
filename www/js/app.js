ce772b5522cb46a834930e8aa58f932a6721d127
Dave Arter <davea@mysociety.org>
2017-06-13 13:33:29 +0100

    Add HELP_DISABLED config key to remove help button from UI

--- www/js/app.js
+++ www/js/app.js
@@ -335,7 +335,9 @@ var tpl = {
                 FMS.checkOnlineStatus();
                 FMS.loadCurrentDraft();
                 FMS.checkLoggedInStatus();
-                FMS.setupHelp();
+                if (!CONFIG.HELP_DISABLED) {
+                    FMS.setupHelp();
+                }
 
                 Backbone.history.start();
                 if ( navigator && navigator.splashscreen ) {
@@ -343,7 +345,9 @@ var tpl = {
                 } else {
                     $('#load-screen').hide();
                 }
-                $('#display-help').show();
+                if (!CONFIG.HELP_DISABLED) {
+                    $('#display-help').show();
+                }
             });
         }
     });
